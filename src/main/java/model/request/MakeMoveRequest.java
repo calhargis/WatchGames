1c3efa63a1a1102dadc3819f6dbaa05fcea839ad
calhargis <calhargis1@gmail.com>
2024-05-26 13:58:58 -0600

    Completed MakeMoveRequest and MakeMoveResponse

--- src/main/java/model/request/MakeMoveRequest.java
+++ src/main/java/model/request/MakeMoveRequest.java
@@ -1,4 +1,40 @@
 package model.request;
 
 public class MakeMoveRequest {
+    private String gameId;
+    private String playerId;
+    private int row;
+    private int col;
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
--- src/main/java/model/response/MakeMoveResponse.java
+++ src/main/java/model/response/MakeMoveResponse.java
@@ -1,4 +1,49 @@
 package model.response;
 
 public class MakeMoveResponse {
+    private String status;
+    private String message;
+    private String[][] board;
+    private String winner;
+    private boolean draw;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(String[][] board) {
+        this.board = board;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public boolean isDraw() {
+        return draw;
+    }
+
+    public void setDraw(boolean draw) {
+        this.draw = draw;
+    }
 }
