dc2040097c7d44b4de64dd0aa7e52bda470c81d9
calhargis <calhargis1@gmail.com>
2024-06-06 14:28:38 -0600

    added empty watch project to front-end

---
+++ front-end/WatchGamesFrontEnd/WatchGamesFrontEnd Watch App/WatchGamesFrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchGamesFrontEndApp.swift
+//  WatchGamesFrontEnd Watch App
+//
+//  Created by Cal Hargis on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchGamesFrontEnd_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ front-end/WatchGamesFrontEnd/WatchGamesFrontEnd/WatchGamesFrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchGamesFrontEndApp.swift
+//  WatchGamesFrontEnd
+//
+//  Created by Cal Hargis on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchGamesFrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
